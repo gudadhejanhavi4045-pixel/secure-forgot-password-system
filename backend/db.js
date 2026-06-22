fa79927ee7016bbef8616fffbbd74db15b4b18a1
janhavigudadhe04 <154574260+janhavigudadhe04@users.noreply.github.com>
2026-06-23 01:58:13 +0530

    fix mysql closed connection

--- backend/db.js
+++ backend/db.js
@@ -1,26 +1,28 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+
+const db = mysql.createPool({
 
     host: process.env.DB_HOST,
+
     user: process.env.DB_USER,
+
     password: process.env.DB_PASSWORD,
+
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+
+    port: process.env.DB_PORT,
+
+    waitForConnections: true,
+
+    connectionLimit: 10,
+
+    queueLimit: 0
 
 });
 
 
-db.connect((err)=>{
-
-    if(err){
-        console.log("Database Error:", err);
-    }
-    else{
-        console.log("MYSQL Connected");
-    }
-
-});
+console.log("MYSQL pool created");
 
 
 module.exports = db;
